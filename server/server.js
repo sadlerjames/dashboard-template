16ab75fc3fdef93b654f5dc9cf59ae1cb69b9e39
SadlerJ <28812304+sadlerjames@users.noreply.github.com>
2019-06-14 22:29:05 +0100

    setup basic express structure

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 3000
+
+//initalise express and routes
+app.use(express.static('../client'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//require the api
+//require(__dirname + './routes')(app, bodyParser);
+
+//send file
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + './index.html');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
