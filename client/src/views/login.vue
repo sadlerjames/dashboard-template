1924e4b3f02d99e3e9c40a59b855d7bb3e312f0b
SadlerJ <28812304+sadlerjames@users.noreply.github.com>
2019-07-03 17:35:45 +0100

    Revert "worked on login from"
    This reverts commit dafb37e5e20a0ac332ef6bc62eef9d7f88ff5a38.

--- client/src/views/login.vue
+++ client/src/views/login.vue
@@ -1,46 +1,17 @@
 <template>
   <div>
-    <b-form @submit="onSubmit" v-if="show">
-      <b-form-group id="email-group" type="email" label="Email Address:" label-for="email">
-        <b-form-input
-          id="email"
-          v-model="form.email"
-          required
-          placeholder="Enter your email address"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="password-group" type="password" label="Password:" label-for="password">
-        <b-form-input
-          id="password"
-          v-model="form.password"
-          required
-          placeholder="Enter your password"
-        ></b-form-input>
-      </b-form-group>
-      <b-button type="submit" variant="primary">Login</b-button>
-    </b-form>
+    <b-form-input v-model="text" placeholder="Email:"></b-form-input>
+    <div class="mt-2">Value: {{ email }}</div>
   </div>
 </template>
 
 <script>
-import auth from "../auth";
-
 export default {
-  name: "app",
-  data() {
+  name: 'app',
+  data () {
     return {
-      form: {
-        email: "",
-        password: ""
-      }
-    };
-  },
-  methods: {
-    onSubmit(evt) {
-      evt.preventDefault()
-      alert(JSON.stringify(this.form))
+      email: ''
     }
   }
-};
+}
 </script>
