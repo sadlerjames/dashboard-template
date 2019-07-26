2cb31cc892ec6ea93b9be5780ee7e64897c2ef07
SadlerJ <28812304+sadlerjames@users.noreply.github.com>
2019-07-26 14:54:17 +0100

    moved sequelize inside server

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const sequelize = require('../../utils/sequelize').sequelize;
+const sequelize = require('../utils/sequelize').sequelize;
 
 const Model = Sequelize.Model;
 class User extends Model { }
